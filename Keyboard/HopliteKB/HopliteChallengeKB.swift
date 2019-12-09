3c34cdebc5970b22e08661600419136928ee0ac3
jeremymarch <jmarch@gradcenter.cuny.edu>
2019-12-08 19:08:34 -0500

    simplify diacritic functions

--- Keyboard/HopliteKB/HopliteChallengeKB.swift
+++ Keyboard/HopliteKB/HopliteChallengeKB.swift
@@ -135,119 +135,101 @@ class HopliteChallengeKB: KeyboardViewController {
         super.viewWillAppear(animated)
     }
     
+    func sendButton(button:Int)
+    {
+        if button > 0 && button < 10
+        {
+            print("a")
+            let key = self.layout?.viewForKey(self.keyboard.pages[0].rows[0][button])
+            print("b")
+            key!.sendActions(for: .touchUpInside)
+            print("c")
+        }
+    }
+    
+    func diacriticPressed(accent:Int, context:String) -> String
+    {
+        assert(context.count > 0, "Cannot accent empty string")
+        assert(unicodeMode == 3, "Invalid Unicode Mode for Hoplite Challenge")
+        if context.count < 1
+        {
+            return ""
+        }
+        //accentSyllable(&context?.utf16, context.count, &context.count, 1, false);
+        /*
+         let bufferSize = 200
+         var nameBuf = [Int8](repeating: 0, count: bufferSize) // Buffer for C string
+         nameBuf[0] = Int8(context![context!.index(before: context!.endIndex)])
+         accentSyllableUtf8(&nameBuf, 1, false)
+         let name = String(cString: nameBuf)
+         */
+        
+        let combiningChars = [COMBINING_BREVE,COMBINING_GRAVE,COMBINING_ACUTE,COMBINING_CIRCUMFLEX,COMBINING_MACRON,COMBINING_DIAERESIS,COMBINING_SMOOTH_BREATHING,COMBINING_ROUGH_BREATHING,COMBINING_IOTA_SUBSCRIPT]
+        
+        // 1. make a buffer for the C string
+        let bufferSize16 = 12 //5 is max, for safety
+        var buffer16 = [UInt16](repeating: 0, count: bufferSize16)
+        
+        // 2. figure out how many characters to send
+        var lenToSend = 1
+        let maxCombiningChars = 5
+        for a in (context.unicodeScalars).reversed()
+        {
+            if lenToSend < maxCombiningChars && combiningChars.contains(Int(a.value))
+            {
+                lenToSend += 1
+            }
+            else
+            {
+                break
+            }
+        }
+        
+        // 3. fill the buffer
+        let suf = context.unicodeScalars.suffix(lenToSend)
+        var j = 0
+        for i in (1...lenToSend).reversed()
+        {
+            buffer16[j] = UInt16(suf[suf.index(suf.endIndex, offsetBy: -i)].value)
+            j += 1
+        }
+        var len16:Int32 = Int32(lenToSend)
+
+        print("len: \(len16), accent pressed, umode: \(unicodeMode)")
+        
+        accentSyllable(&buffer16, 0, &len16, Int32(accent), true, Int32(unicodeMode))
+        
+        return String(utf16CodeUnits: buffer16, count: Int(len16))
+    }
+    
     override func keyPressed(_ key: Key) {
-        let textDocumentProxy = self.textDocumentProxy
-        let keyOutput = key.outputForCase(self.shiftState.uppercase())
         
         if key.type == .diacritic
         {
             let whichAccent = (self.shiftState == .disabled) ? key.lowercaseOutput : key.uppercaseOutput
 
-            var accent = -1
-            if whichAccent == "acute" //1 acute
-            {
-                accent = 1
-            }
-            else if whichAccent == "circum" //2 circumflex
-            {
-                accent = 2
-            }
-            else if whichAccent == "grave" //3 grave
-            {
-                accent = 3
-            }
-            else if whichAccent == "macron" //macron
-            {
-                accent = 4
-            }
-            else if whichAccent == "rough" //rough breathing
-            {
-                accent = 5
-            }
-            else if whichAccent == "smooth" //smooth breathing
-            {
-                accent = 6
-            }
-            else if whichAccent == "iotasub" //iota subscript
-            {
-                accent = 7
-            }
-            else if whichAccent == "parens" //surrounding parentheses
-            {
-                accent = 8
-            }
-            else if whichAccent == "diaeresis" //diaeresis
-            {
-                accent = 9
-            }
-            else if whichAccent == "breve" //breve
-            {
-                accent = 10
-            }
-            else
-            {
+            let diacritics = ["acute", "circum", "grave", "macron", "rough", "smooth", "iotasub", "parens", "diaeresis", "breve"]
+            
+            guard let accent = diacritics.firstIndex(of: whichAccent!), accent > -1, accent < 10
+                else {
+                    assertionFailure("Invalid Accent")
                 return
             }
             
-            let context = self.textDocumentProxy.documentContextBeforeInput
-            let len = context?.count
-            if len == nil || len! < 1
+            if let context = textDocumentProxy.documentContextBeforeInput, context.count > 0
             {
-                return
+                //accents are 1-10, so add 1
+                let newLetter = diacriticPressed(accent: accent + 1, context: context)
+                
+                (textDocumentProxy as UIKeyInput).deleteBackward() //seems to include any combining chars, but not in MSWord!
+                (textDocumentProxy as UIKeyInput).insertText(newLetter)
             }
-            
-            //accentSyllable(&context?.utf16, context.count, &context.count, 1, false);
-            /*
-             let bufferSize = 200
-             var nameBuf = [Int8](repeating: 0, count: bufferSize) // Buffer for C string
-             nameBuf[0] = Int8(context![context!.index(before: context!.endIndex)])
-             accentSyllableUtf8(&nameBuf, 1, false)
-             let name = String(cString: nameBuf)
-             */
-            
-            let combiningChars = [COMBINING_BREVE,COMBINING_GRAVE,COMBINING_ACUTE,COMBINING_CIRCUMFLEX,COMBINING_MACRON,COMBINING_DIAERESIS,COMBINING_SMOOTH_BREATHING,COMBINING_ROUGH_BREATHING,COMBINING_IOTA_SUBSCRIPT]
-            
-            // 1. make a buffer for the C string
-            let bufferSize16 = 12 //5 is max, for safety
-            var buffer16 = [UInt16](repeating: 0, count: bufferSize16)
-            
-            // 2. figure out how many characters to send
-            var lenToSend = 1
-            let maxCombiningChars = 5
-            for a in (context!.unicodeScalars).reversed()
-            {
-                if lenToSend < maxCombiningChars && combiningChars.contains(Int(a.value))
-                {
-                    lenToSend += 1
-                }
-                else
-                {
-                    break
-                }
-            }
-            
-            // 3. fill the buffer
-            let suf = context!.unicodeScalars.suffix(lenToSend)
-            var j = 0
-            for i in (1...lenToSend).reversed()
-            {
-                buffer16[j] = UInt16(suf[suf.index(suf.endIndex, offsetBy: -i)].value)
-                j += 1
-            }
-            var len16:Int32 = Int32(lenToSend)
-
-            print("len: \(len16), accent pressed, umode: \(unicodeMode)")
-            
-            accentSyllable(&buffer16, 0, &len16, Int32(accent), true, Int32(unicodeMode))
-            
-            let newLetter = String(utf16CodeUnits: buffer16, count: Int(len16))
-            
-            (textDocumentProxy as UIKeyInput).deleteBackward() //seems to include any combining chars, but not in MSWord!
-            (textDocumentProxy as UIKeyInput).insertText("\(newLetter)")
         }
         else if key.type == .multipleforms
         {
             print("multiple forms pressed")
+            //get output before setting it to comma
+            let keyOutput = key.outputForCase(self.shiftState.uppercase())
             if let keyView = self.layout?.viewForKey(key)
             {
                 key.lowercaseKeyCap = ","
@@ -258,6 +240,7 @@ class HopliteChallengeKB: KeyboardViewController {
         }
         else
         {
+            let keyOutput = key.outputForCase(self.shiftState.uppercase())
             textDocumentProxy.insertText(keyOutput)
         }
     }
@@ -281,4 +264,3 @@ class HopliteChallengeKB: KeyboardViewController {
         return defaults?.bool(forKey: kKeyboardClicks) ?? true
     }
 }
-
